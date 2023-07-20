47969bc4aecab5f7cf1002b9dfed3bfd318b8b37
1004joonsuk <1004joonsuk@naver.com>
2023-07-20 14:04:14 +0900

    master[S09P12A508-199]: 백엔드 프로젝트 생성

---
+++ backend/varabc/src/main/java/com/varabc/member/VarabcApplication.java
@@ -0,0 +1,13 @@
+package com.varabc.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VarabcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VarabcApplication.class, args);
+	}
+
+}
