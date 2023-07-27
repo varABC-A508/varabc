eb5f73c296cb7e2b238151f0a6ec19392ea88fa0
1004joonsuk <1004joonsuk@naver.com>
2023-07-27 16:42:28 +0900

    feat/java-validate-server[S09P12A508-393]: html에서 json데이터 전송 시, 코드 채점 프로그램 개발, 스레드를 활용한 메모리, 시간 제한 기능 구현, 주석 문제 해결, 하지만 여전히 측정되는 메모리 사이즈 오류가 있어보임.

--- validate_server/JavaValidation/problemsolvingjava/Main.java
+++ validate_server/JavaValidation/problemsolvingjava/Main.java
@@ -1,22 +1,74 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	
+	private static BufferedReader br;
+	private static int N,answer;
+	private static StringTokenizer st;
+	private static int[] T;
+	private static int[] P;
+	private static int[] dp;
+    private static int[][] mtest;
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		int n = Integer.parseInt(br.readLine());
-		
-		int[] dp = new int[1001];
-		
-		dp[1] = 1;
-		dp[2] = 2;
-		
-		for (int i = 3; i < n + 1; i++) {
-			dp[i] = (dp[i-1] + dp[i-2]) % 10_007;
+		input();
+		//백트래킹으로 먼저 짜봅니다.
+		//현재 날짜에서부터 현재상담을 고르고 다음날로갈지, 안고르고 갈지를 결정하자.
+		dp = new int[N];
+        mtest = new int[1000][10000];
+		Arrays.fill(dp, -1);	//디피 배열의 차원수는 백트함수의 인자개수, -1로 초기화.
+		System.out.println(findMaxDP(0));
+	}
+
+	//백트를 먼저 짜고 디피를 넣어주자.
+	private static void findMax(int curday,int cost) {
+		//기저조건, 목표 날짜에 도달하면 answer와 비교하고 종료,
+		if (curday==N) {
+			if (answer<cost) {
+				answer=cost;
+			}
+			return;
 		}
-		
-		System.out.println(dp[n]);
+		int max=Integer.MAX_VALUE;
+		//목표날짜를 넘어갔으면 정답이 될 수 없는 값을 더해주고 종료
+		if (curday>N) {
+			cost-=1600000000;	//정답이 안되게 아주 낮은 값을 줘버리는겨
+			return;
+		}
+		//오늘 고를 수 있는애를 고르고가던가
+		findMax(curday+T[curday], cost+P[curday]);
+		//안고르고 내일로 가던가
+		findMax(curday+1, cost);
+	}
+	
+	//탑 다운 디피
+	private static int findMaxDP(int curday) {
+		//기저조건, 목표 날짜에 도달하면 0을 리턴
+		if (curday==N) {
+			return 0;
+		}
+		//목표날짜를 넘어갔으면 정답이 될 수 없는 값을 더해주고 종료
+		if (curday>N) {
+			return -1600000000; //정답이 안되게 아주 낮은 값을 줘버리는겨
+		}
+		int temp=Math.max(findMaxDP(curday+1), findMaxDP(curday+T[curday])+P[curday]);
+		if (dp[curday]==-1) {
+			dp[curday]=temp;
+			return temp;
+		}else {
+			return dp[curday];
+		}
+	}
+
+	private static void input() throws NumberFormatException, IOException {
+		br= new BufferedReader(new InputStreamReader(System.in));
+		N=Integer.parseInt(br.readLine());
+		T= new int[N];
+		P= new int[N];
+		for (int i = 0; i < N; i++) {
+			st= new StringTokenizer(br.readLine());
+			T[i]=Integer.parseInt(st.nextToken());
+			P[i]=Integer.parseInt(st.nextToken());
+		}
 	}
 }
--- validate_server/JavaValidation/problemsolvingjava/src/main/java/com/problemsolvingjava/validate/controller/CodeExecutionController.java
+++ validate_server/JavaValidation/problemsolvingjava/src/main/java/com/problemsolvingjava/validate/controller/CodeExecutionController.java
@@ -2,6 +2,13 @@ package com.problemsolvingjava.validate.controller;
 
 import com.problemsolvingjava.validate.domain.dto.SubmitDto;
 import com.problemsolvingjava.validate.domain.dto.TestCaseDto;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import org.springframework.web.bind.annotation.*;
 import java.io.*;
 
@@ -11,85 +18,162 @@ public class CodeExecutionController {
     @PostMapping("/validate")
     public String executeCode(@RequestBody SubmitDto submitDto)
             throws IOException, InterruptedException {
-        System.out.println(submitDto.getCode());
-        // 1. 코드를 .java 파일로 저장하기
+        // 파일이름 정하고 파일에 읽기, 덮어쓰는 방식으로 하자
         String filename = "Main.java";
-        FileWriter writer = new FileWriter(filename);
+        //컴파일러 인코더 설정, 근데 한글 주석은 여전히 깨짐
+        System.setProperty("file.encoding", "UTF-8");
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filename), "UTF-8"));
         writer.write(submitDto.getCode());
         writer.close();
+        // 2. 코드 컴파일, 프로세스 빌더
+        ProcessBuilder compileProcessBuilder = new ProcessBuilder("javac", "-encoding", "UTF-8", filename);
+        Process compileProcess = compileProcessBuilder.start();
 
-        // 2. 코드 컴파일하기
-        Process compileProcess = Runtime.getRuntime().exec("javac " + filename);
-        try {
-            int compileResult = compileProcess.waitFor();
-            if (compileResult != 0) {
-                return "Compile error!";
-            }
-        } catch (InterruptedException e) {
-            return "Compilation interrupted: " + e.getMessage();
+        int compileResult = compileProcess.waitFor();
+        if (compileResult != 0) {
+            System.out.println("error");
+            return "Compile error!";
         }
 
-        System.out.println("컴파일 완료");
+        System.out.println("Compilation successful");
 
-        System.out.println(submitDto.getTestCases());
-
-        long time = 0;
-        long memory = 0;
-
-        Runtime runtime = Runtime.getRuntime();
+        // 3. 각각의 테케로 실행하기
+        long maxElapsedTime = 0;
 
         for (TestCaseDto testCaseDto : submitDto.getTestCases()) {
-            // 초기 시간을 저장합니다.
-            long startTime = System.nanoTime();
-            // 초기 전체 메모리와 가용 메모리를 가져옵니다.
-            long initialMemory = runtime.totalMemory() - runtime.freeMemory();
-
-            // 3. 코드 실행하기
-            System.out.println(testCaseDto.getInputData());
-            Process runProcess = new ProcessBuilder("java", "Main")
+            //output설정하기
+            File outputFile = new File("output.txt");
+            //메모리제한
+            ProcessBuilder runProcessBuilder = new ProcessBuilder("java", "-Xmx512m", "Main")
                     .redirectInput(ProcessBuilder.Redirect.PIPE)
-                    .redirectOutput(ProcessBuilder.Redirect.PIPE)
-                    .start();
+                    .redirectOutput(outputFile)
+                    .directory(new File(filename).getParentFile());
 
-            // Provide input to the process
-            runProcess.getOutputStream().write(testCaseDto.getInputData().getBytes());
-            runProcess.getOutputStream().flush();
-            runProcess.getOutputStream().close();
+            //프로세스 실행
+            long startTime = System.nanoTime();
+            Process runProcess = runProcessBuilder.start();
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(runProcess.getInputStream()));
-            StringBuilder output = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                output.append(line).append("\n");
-            }
-            int runResult = runProcess.waitFor();
+            // PID를 얻습니다.
+            long pid = runProcess.pid();
 
-            // 코드 실행 후 시간을 저장합니다.
-            long endTime = System.nanoTime();
 
-            // 코드 실행 후 전체 메모리와 가용 메모리를 다시 측정합니다.
-            long finalMemory = runtime.totalMemory() - runtime.freeMemory();
 
-            // 코드 실행 시간을 측정합니다.
-            long elapsedTime = endTime - startTime;
 
-            // 코드가 실행되며 사용한 시간을 측정합니다.
-            long usedMemory = finalMemory - initialMemory;
-
-            if (runResult != 0) {
-                return "Runtime error!";
-            }
-
-            // 4. 테스트 케이스 확인하기
-            if (output.toString().trim().equals(testCaseDto.getExpectedData())) {
-                System.out.println("Test passed!");
-                time = Math.max(time, elapsedTime);
-                memory = Math.max(memory, usedMemory);
+            // 메모리 사용량을 주기적으로 확인하는 스레드를 시작합니다.
+            String os = System.getProperty("os.name").toLowerCase();
+            if (os.contains("win")) {
+                //윈도우용 코드
+                Thread memoryMonitor = new Thread(() -> {
+                    long maxMemoryUsage = 0;
+                    try {
+                        while (runProcess.isAlive()) {
+                            ProcessBuilder processBuilder = new ProcessBuilder("cmd.exe", "/c", "tasklist /FI \"PID eq " + pid + "\" /FO CSV /NH");
+                            Process memoryProcess = processBuilder.start();
+                            try (BufferedReader reader = new BufferedReader(new InputStreamReader(memoryProcess.getInputStream()))) {
+                                String line;
+                                while ((line = reader.readLine()) != null) {
+                                    String[] split = line.split(",");
+                                    if (split.length > 4) {
+                                        // 메모리 사용량은 5번째 열에 있습니다.
+                                        // 그것을 파싱하고 킬로바이트 단위로 출력합니다.
+                                        long memoryUsage = Long.parseLong(split[4].replaceAll("\"", "").replaceAll(",", "").trim());
+                                        maxMemoryUsage = Math.max(maxMemoryUsage, memoryUsage);
+                                    }
+                                }
+                            }
+                            // 매 0.1초마다 메모리 사용량을 확인합니다.
+                            Thread.sleep(100);
+                        }
+                    } catch (InterruptedException | IOException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Maximum memory usage: " + maxMemoryUsage + "KB");
+                });
+                memoryMonitor.start();
             } else {
+                //리눅스용 코드
+                Thread memoryMonitor = new Thread(() -> {
+                    long maxMemoryUsage = 0;
+                    try {
+                        while (runProcess.isAlive()) {
+                            ProcessBuilder processBuilder = new ProcessBuilder("bash", "-c", "ps -p " + pid + " -o rss");
+                            Process memoryProcess = processBuilder.start();
+                            try (BufferedReader reader = new BufferedReader(new InputStreamReader(memoryProcess.getInputStream()))) {
+                                String line;
+                                while ((line = reader.readLine()) != null) {
+                                    // 첫 번째 줄은 헤더이므로 건너뛰기
+                                    if (line.trim().startsWith("RSS")) {
+                                        continue;
+                                    }
+                                    // 메모리 사용량은 킬로바이트 단위로 출력됩니다.
+                                    long memoryUsage = Long.parseLong(line.trim());
+                                    maxMemoryUsage = Math.max(maxMemoryUsage, memoryUsage);
+                                }
+                            }
+                            // 매 0.1초마다 메모리 사용량을 확인합니다.
+                            Thread.sleep(100);
+                        }
+                    } catch (InterruptedException | IOException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Maximum memory usage: " + maxMemoryUsage + "KB");
+                });
+                memoryMonitor.start();
+            }
+
+
+
+
+
+            //input 입력해주기
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(runProcess.getOutputStream()));
+            bufferedWriter.write(testCaseDto.getInputData());
+            bufferedWriter.close();
+
+            Future<Boolean> future = Executors.newSingleThreadExecutor().submit(() -> {
+                while(runProcess.isAlive()) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        runProcess.destroy();
+                        return false;
+                    }
+                }
+                return true;
+            });
+
+            try {
+                future.get(2, TimeUnit.SECONDS); // If the task takes more than 2 seconds, a TimeoutException will be thrown
+            } catch (TimeoutException e) {
+                future.cancel(true); // In case it is still running, interrupt the thread
+                if (runProcess.isAlive()) {
+                    runProcess.destroyForcibly(); // Terminate the process forcibly.
+                }
+                System.out.println("timeout");
+                return "Timeout error!";
+            } catch (InterruptedException | ExecutionException e) {
+                System.out.println("other error");
+                return "Execution error!";
+            }
+
+            // 예상 결과랑 결과 비교하기->toString으로 비교하면 오버헤드가 너무 커짐
+            String output = new String(Files.readAllBytes(outputFile.toPath()), "UTF-8");
+            if (!output.trim().equals(testCaseDto.getExpectedData())) {
+                System.out.println("Test failed");
                 return "Test failed!";
             }
+
+            long endTime = System.nanoTime();
+            long elapsedTime = endTime - startTime;
+            maxElapsedTime = Math.max(maxElapsedTime, elapsedTime);
+            System.out.println(elapsedTime/ 1_000_000);
+            System.out.println("Test passed");
         }
 
-        return "All test cases passed! time : " + time / 1_000_000 + "ms, memory : " + memory / 1024 + "Kb";
+        //다쓰면 지워버리기~
+        Files.deleteIfExists(Paths.get(filename));
+        Files.deleteIfExists(Paths.get(filename.substring(0, filename.lastIndexOf('.')) + ".class"));
+        System.out.println(maxElapsedTime);
+        return "All tests passed! Time: " + maxElapsedTime / 1_000_000;
     }
 }
