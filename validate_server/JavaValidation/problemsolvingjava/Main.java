3d63e322d739eb2b0cb4beab6db05733a6878ba7
1004joonsuk <1004joonsuk@naver.com>
2023-07-28 16:21:23 +0900

    feat/java-validate-server[S09P12A508-393]: 채점 프로그램에서 시간제한, 메모리제한 받을 수 있게 dto 수정

--- validate_server/JavaValidation/problemsolvingjava/Main.java
+++ validate_server/JavaValidation/problemsolvingjava/Main.java
@@ -1,74 +1,38 @@
-import java.io.*;
-import java.util.*;
+#include <iostream>
+#include <vector>
+using namespace std;
 
-public class Main {
-	private static BufferedReader br;
-	private static int N,answer;
-	private static StringTokenizer st;
-	private static int[] T;
-	private static int[] P;
-	private static int[] dp;
-    private static int[][] mtest;
+int N;
+vector<int> timelist, costlist;
+vector<int> dp;
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		input();
-		//백트래킹으로 먼저 짜봅니다.
-		//현재 날짜에서부터 현재상담을 고르고 다음날로갈지, 안고르고 갈지를 결정하자.
-		dp = new int[N];
-        mtest = new int[1000][10000];
-		Arrays.fill(dp, -1);	//디피 배열의 차원수는 백트함수의 인자개수, -1로 초기화.
-		System.out.println(findMaxDP(0));
-	}
+int dpTrack(int day) {
+    if (day > N + 1) {
+        return -100000000;
+    }
+    if (day == N + 1) {
+        return 0;
+    }
+    if (dp[day] != -1) {
+        return dp[day];
+    }
+    dp[day] = max(dpTrack(day + 1), dpTrack(day + timelist[day]) + costlist[day]);
+    return dp[day];
+}
 
-	//백트를 먼저 짜고 디피를 넣어주자.
-	private static void findMax(int curday,int cost) {
-		//기저조건, 목표 날짜에 도달하면 answer와 비교하고 종료,
-		if (curday==N) {
-			if (answer<cost) {
-				answer=cost;
-			}
-			return;
-		}
-		int max=Integer.MAX_VALUE;
-		//목표날짜를 넘어갔으면 정답이 될 수 없는 값을 더해주고 종료
-		if (curday>N) {
-			cost-=1600000000;	//정답이 안되게 아주 낮은 값을 줘버리는겨
-			return;
-		}
-		//오늘 고를 수 있는애를 고르고가던가
-		findMax(curday+T[curday], cost+P[curday]);
-		//안고르고 내일로 가던가
-		findMax(curday+1, cost);
-	}
-	
-	//탑 다운 디피
-	private static int findMaxDP(int curday) {
-		//기저조건, 목표 날짜에 도달하면 0을 리턴
-		if (curday==N) {
-			return 0;
-		}
-		//목표날짜를 넘어갔으면 정답이 될 수 없는 값을 더해주고 종료
-		if (curday>N) {
-			return -1600000000; //정답이 안되게 아주 낮은 값을 줘버리는겨
-		}
-		int temp=Math.max(findMaxDP(curday+1), findMaxDP(curday+T[curday])+P[curday]);
-		if (dp[curday]==-1) {
-			dp[curday]=temp;
-			return temp;
-		}else {
-			return dp[curday];
-		}
-	}
+int main() {
+    cin.tie(nullptr);
 
-	private static void input() throws NumberFormatException, IOException {
-		br= new BufferedReader(new InputStreamReader(System.in));
-		N=Integer.parseInt(br.readLine());
-		T= new int[N];
-		P= new int[N];
-		for (int i = 0; i < N; i++) {
-			st= new StringTokenizer(br.readLine());
-			T[i]=Integer.parseInt(st.nextToken());
-			P[i]=Integer.parseInt(st.nextToken());
-		}
-	}
+    cin >> N;
+    timelist.resize(N + 1);
+    costlist.resize(N + 1);
+    dp.resize(N + 1, -1);
+
+    for (int i = 1; i <= N; i++) {
+        cin >> timelist[i] >> costlist[i];
+    }
+
+    cout << dpTrack(1) << "\n";
+
+    return 0;
 }
