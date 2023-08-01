bfd9706ecd03029d5b4fd2bdc812f9aab407508b
heejo <heejo515@naver.com>
2023-08-01 10:33:57 +0900

    feat/rds-s3-crud [S09P12A508-507]: rds testcase table에 데이터 text로 저장

---
+++ backend/varabc/src/main/java/com/varabc/problem/repository/TestcaseRepository.java
@@ -0,0 +1,9 @@
+package com.varabc.problem.repository;
+
+import com.varabc.problem.domain.entity.ProblemRestrictionEntity;
+import com.varabc.problem.domain.entity.TestcaseEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestcaseRepository extends JpaRepository<TestcaseEntity, Long> {
+
+}
