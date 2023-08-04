f8eb8f7401c2bbaab0d8562bbfdcf93f457ba33d
GardenJB <ohjb93@gmail.com>
2023-08-04 17:02:40 +0900

    feat/list-friends [S09P12A508-496]: 친구 목록 기본 틀 완성 css와 목록 상태 변경 수정 필요

--- frontend/varabc/src/components/common/list/FriendsList.js
+++ frontend/varabc/src/components/common/list/FriendsList.js
@@ -19,63 +19,64 @@ const FriendsList = ({ isMyFriends }) => {
     ];
 
     setFriends(exampleFriends);
+
+    // // 백엔드 서버로부터 친구 목록을 가져오는 API 호출
+    // axios.get('/api/friends')
+    //   .then(response => {
+    //     // 가져온 친구 목록을 상태 변수에 설정
+    //     setFriends(response.data);
+    //   })
+    //   .catch(error => {
+    //     console.error('Error fetching friends:', error);
+    //   });
   };
-  // // 백엔드 서버로부터 친구 목록을 가져오는 API 호출
-  // axios.get('/api/friends')
-  //   .then(response => {
-  //     // 가져온 친구 목록을 상태 변수에 설정
-  //     setFriends(response.data);
-  //   })
-  //   .catch(error => {
-  //     console.error('Error fetching friends:', error);
-  //   });
-};
 
-const myRequest = () => {
-  console.log('친구 요청 목록');
-  // 예시 데이터로 임의의 친구 요청 목록 생성
-  const exampleRequests = [
-    { id: 5, profile: '프로필5', teer: '티어5', name: '이름5' },
-    { id: 6, profile: '프로필6', teer: '티어6', name: '이름6' },
-  ];
+  const myRequest = () => {
+    console.log('친구 요청 목록');
+    // 예시 데이터로 임의의 친구 요청 목록 생성
+    const exampleRequests = [
+      { id: 5, profile: '프로필5', teer: '티어5', name: '이름5' },
+      { id: 6, profile: '프로필6', teer: '티어6', name: '이름6' },
+    ];
 
-  setFriends(exampleRequests);
+    setFriends(exampleRequests);
 
-  // axios.get('/api/friends/request')
-  //   .then(res => {
-  //     setFriends(res.data);
-  //   })
-};
+    // axios.get('/api/friends/request')
+    //   .then(res => {
+    //     setFriends(res.data);
+    //   })
+  };
 
-useEffect(() => {
-  { isMyFriends ? myFriend() : myRequest() }
-}, []);
+  useEffect(() => {
+    { isMyFriends ? myFriend() : myRequest() }
+  }, []);
 
-return (
-  <div>
-    {friends.length === 0 ? (
-      <p>목록이 없습니다.</p>
-    ) : (
-      <ul>
-        {friends.map(friend => (
-          <li key={friend.id}>
-            <p>{friend.profile}</p>
-            <p>{friend.teer}</p>
-            <p>{friend.name}</p>
-            {isMyFriends ? (
-              <SmButton text={"친구 삭제"} bgColor={"red"} />
-            ) : (
-              <div>
-                <SmButton text={"요청 수락"} bgColor={"green"} />
-                <SmButton text={"요청 거절"} bgColor={"red"} />
-              </div>
-            )}
-          </li>
-        ))}
-      </ul>
-    )}
-  </div>
-)
+  return (
+    <div>
+      {friends.length === 0 ? (
+        <p>목록이 없습니다.</p>
+      ) : (
+        <ul>
+          {friends.map(friend => (
+            // 임시 값 수정 필요
+            <li key={friend.id}>
+              <p>{friend.profile}</p>
+              <p>{friend.teer}</p>
+              <p>{friend.name}</p>
+              {isMyFriends ? (
+                <SmButton text={"친구 삭제"} bgColor={"red"} />
+              ) : (
+                <div>
+                  <SmButton text={"요청 수락"} bgColor={"green"} />
+                  <SmButton text={"요청 거절"} bgColor={"red"} />
+                </div>
+              )}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  )
 };
 
 export default FriendsList;
