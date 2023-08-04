b8ea31bee9df50ae44cd21a6ed5984e882746d3e
GardenJB <ohjb93@gmail.com>
2023-08-04 16:28:18 +0900

    feat/list-friends : 임시 저장

--- frontend/varabc/src/pages/myPage/Friends.js
+++ frontend/varabc/src/pages/myPage/Friends.js
@@ -1,4 +1,26 @@
+import React, { useState } from 'react';
+import FriendsList from '../../components/common/list/FriendsList';
+import SmButton from '../../components/common/Button/SmButton';
 
 export const Friends = () => {
-    return (<div>친구 화면</div>);
+
+  const [isFriendslist, setIsFriendslist] = useState(true);
+
+  const FriendList = () => {
+    setIsFriendslist(true);
+  }
+
+  const FriendRequestList = () => {
+    setIsFriendslist(false);
+  }
+
+  return (
+    <div>
+      친구 화면
+      <SmButton onClick={FriendList} text={'친구 목록'} bgColor={'basic'} />
+      <SmButton onClick={FriendRequestList} text={'친구 요청'} bgColor={'basic'} />
+      
+      <FriendsList isMyFriends={isFriendslist} />
+    </div>
+  );
 };
