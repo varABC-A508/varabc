b9a1c20341c20ae7e2545e37748899f7234d59fe
김영서 <kys981207@naver.com>
2023-08-09 15:10:07 +0900

    feature/jwt_and_spring_security [S09P12A508-767]: jwt 토큰 사용을 위한 JwtServiceImpl...

--- backend/varabc/src/main/java/com/varabc/member/controller/MemberController.java
+++ backend/varabc/src/main/java/com/varabc/member/controller/MemberController.java
@@ -1,30 +1,21 @@
 package com.varabc.member.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.varabc.member.domain.dto.LoginResponseDto;
+import com.varabc.jwt.service.JwtService;
 import com.varabc.member.domain.entity.Member;
-import com.varabc.member.repository.MemberRepository;
-import java.util.Arrays;
 
 import com.varabc.member.service.GoogleLoginService;
 import com.varabc.member.service.KakaoLoginService;
 import com.varabc.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.converter.FormHttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/member")
@@ -34,9 +25,10 @@ public class MemberController {
     private final GoogleLoginService googleLoginService;
     private final MemberService memberService;
     private final KakaoLoginService kakaoLoginService;
+    private final JwtService jwtService;
 
     @GetMapping("google-login")
-    public ResponseEntity<Object> loginGoogle(@RequestParam String code){
+    public ResponseEntity<Map<String, Object>> loginGoogle(@RequestParam String code){
         // Access Token 발급
         JsonNode jsonToken = googleLoginService.getAccessToken(code);
 //        System.out.println(jsonToken.toString());
@@ -48,9 +40,33 @@ public class MemberController {
         String expiresTime = jsonToken.get("expires_in").toString();
         // Access Token으로 사용자 정보 반환
         JsonNode userInfo = googleLoginService.getGoogleUserInfo(accessToken);
-        System.out.println(userInfo.toString());
+//        System.out.println(userInfo.toString());
 
-        return ResponseEntity.ok(memberService.saveGoogleMember(userInfo));
+//        return ResponseEntity.ok(memberService.saveGoogleMember(userInfo));
+
+        ////
+//        System.out.println("userInfo : " + userInfo);
+        Member member = memberService.saveGoogleMember(userInfo);
+
+        Map<String, Object> resultMap= new HashMap<>();
+        HttpStatus status=null;
+        try{
+            //고유식별자로 토큰 발급
+            String accessTokenForJwt=jwtService.createAccessToken("memberNo", member.getMemberNo());
+            System.out.println(accessTokenForJwt);
+            String refreshTokenForJwt=jwtService.createRefreshToken("memberNo", member.getMemberNo());
+            System.out.println(refreshTokenForJwt);
+            memberService.saveRefreshToken(member.getMemberNo(), refreshTokenForJwt);
+            //for debug
+            resultMap.put("message", "SUCCESS");
+            resultMap.put("access-token",accessTokenForJwt);
+            resultMap.put("refresh-token",refreshTokenForJwt);
+            status=HttpStatus.ACCEPTED;
+        }catch(Exception e){
+            resultMap.put("message", e.getMessage());
+            status=HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return new ResponseEntity<Map<String, Object>>(resultMap, status);
     }
 
     @GetMapping("kakao-login")
