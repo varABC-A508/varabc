6874b52a49f80de38269e03a9c20e7344dbb51ca
GardenJB <ohjb93@gmail.com>
2023-08-11 14:19:07 +0900

    fix : access-token 저장

--- frontend/varabc/src/components/login/NicknameModal.js
+++ frontend/varabc/src/components/login/NicknameModal.js
@@ -39,14 +39,14 @@ const NicknameModal = ({ isOpen, onClose, onSave }) => {
         <div className="fixed inset-0 bg-black opacity-50 z-40"></div>
         <div className="bg-white p-8 rounded shadow-lg z-50">
           <h2 className="text-2xl font-bold mb-4">닉네임 입력</h2>
-          <input
+          <input className='border'
             type="text"
             value={nickname}
             onChange={(e) => setNickname(e.target.value)}
           />
-          <button className="mt-4 px-4 py-2 bg-red-500 text-white rounded" onClick={checkNickname}>중복 검사</button>
+          <button className="mt-4 px-4 py-2 bg-green text-black rounded z-60" onClick={checkNickname}>중복 검사</button>
           {NicknameCheck ? <p>사용 가능</p> : <p>사용 불가</p>}
-          <button className="mt-4 px-4 py-2 bg-red-500 text-white rounded" onClick={handleSave}>확인</button>
+          <button className="mt-4 px-4 py-2 bg-blue-500 text-black rounded z-60" onClick={handleSave}>확인</button>
         </div>
       </div>
     );
