a1027ebea60592c1ca9d9416876b7531b751adba
이예린 <h2002y@naver.com>
2023-08-12 21:12:34 +0900

    fix: 로그아웃시 로컬 스토리지 nickname 제거

--- frontend/varabc/src/components/common/nav/index.js
+++ frontend/varabc/src/components/common/nav/index.js
@@ -44,25 +44,25 @@ export const Nav = () => {
           티어
         </Link>
       </div>
-      <div className='w-20% pr-10'>
-        {nickname ? (
+      <div className="w-20% pr-10">
+        {(nickname === "null") || (nickname === null) || (nickname === (undefined)) || (nickname === "" )? (
           <div>
-            <div className='text-xl'>환영합니다</div>
-            <div className='text-2xl'>
-              <span className='font-bold'>{nickname}</span>
-              <span>님!</span>
-              <button onClick={logout}>로그아웃</button>
-            </div>
+            <button
+              onClick={handleOpenModal}
+              className="bg-blue-500 text-white px-4 py-2 rounded"
+            >
+              Login
+            </button>
+            <Login isOpen={isModalOpen} onClose={handleCloseModal} />
           </div>
         ) : (
           <div>
-            <button onClick={handleOpenModal} className="bg-blue-500 text-white px-4 py-2 rounded">
-              Login
-            </button>
-            <Login
-              isOpen={isModalOpen}
-              onClose={handleCloseModal}
-            />
+            <div className="text-xl">환영합니다</div>
+            <div className="text-2xl">
+              <span className="font-bold">{nickname}</span>
+              <span>님!</span>
+              <button onClick={logout}>로그아웃</button>
+            </div>
           </div>
         )}
       </div>
