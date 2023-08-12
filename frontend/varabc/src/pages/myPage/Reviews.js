f3462a30a69052796425395e3c82561f7b2e714c
오정빈 <ohjb93@gmail.com>
2023-08-12 19:00:59 +0900

    Feature/review card

--- frontend/varabc/src/pages/myPage/Reviews.js
+++ frontend/varabc/src/pages/myPage/Reviews.js
@@ -1,4 +1,10 @@
+import ReviewCard from "./ReviewCard";
 
 export const Reviews = () => {
-    return (<div>리뷰 화면</div>);
+    return (
+        <div>
+            리뷰 화면
+            <ReviewCard />
+        </div>
+    );
 };
