946b3b3895afe0b04c838736be85783b832e5cb8
이예린 <h2002y@naver.com>
2023-08-13 01:04:10 +0900

    fix: 대기실 사용자 정보 테스트 출력

--- frontend/varabc/src/pages/battle/PlayerProfile.js
+++ frontend/varabc/src/pages/battle/PlayerProfile.js
@@ -3,11 +3,11 @@ const PlayerProfile = ({player}) => {
     return (
         <div className="flex justify-start items-center w-[370px] h-[120px]">
             <div>
-                <img src={player.memberImage} alt="playerProfile" className="w-[120px] h-[120px] rounded-[16px] border-2" />
+                <img src={player.member.memberImage} alt="playerProfile" className="w-[120px] h-[120px] rounded-[16px] border-2" />
             </div>
             <div className="ml-5">
-                <div className="text-white font-bold text-[40px]">{player.memberNickname}</div>
-                <div className="text-white font-bold text-[24px]">#{player.memberNo}</div>
+                <div className="text-white font-bold text-[40px]">{player.member.memberNickname}</div>
+                <div className="text-white font-bold text-[24px]">#{player.member.memberNo}</div>
             </div>
         </div>
     );
--- frontend/varabc/src/pages/battle/TeamWaiting.js
+++ frontend/varabc/src/pages/battle/TeamWaiting.js
@@ -4,19 +4,11 @@ import PlayerProfile from "./PlayerProfile";
 
 
 const TeamWaiting = ({player1, player2, teamNo}) => {
-    useEffect(() => {
-        console.log(`${teamNo} 참가자 1`);
-        console.log(player1);
-    }, [player1]);
-    useEffect(() => {
-        console.log(`${teamNo} 참가자 2`);
-        console.log(player2);
-    }, [player2]);
     return (
         <div className="flex flex-col bg-primaryDark w-[414px] h-[416px] rounded-[25px] p-5 justify-around items-center">
             <div className="text-white text-[48px] font-bold">Team {teamNo}</div>
-            { (player1 && player1.memberNo) ? <PlayerProfile player={player1} /> : <AddPlayerButton /> }
-            { (player2 && player2.memberNo) ? <PlayerProfile player={player2} /> : <AddPlayerButton /> }
+            { (player1 && player1.member.memberNo) ? <PlayerProfile player={player1} /> : <AddPlayerButton /> }
+            { (player2 && player2.member.memberNo) ? <PlayerProfile player={player2} /> : <AddPlayerButton /> }
         </div>
     );
 };
