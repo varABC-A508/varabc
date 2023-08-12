952d6540a7b2c5d67888767986910a0cd1f0cea0
이예린 <h2002y@naver.com>
2023-08-13 00:46:19 +0900

    fix: 대기실 사용자 정보 테스트 출력

--- frontend/varabc/src/pages/battle/TeamWaiting.js
+++ frontend/varabc/src/pages/battle/TeamWaiting.js
@@ -7,16 +7,16 @@ const TeamWaiting = ({player1, player2, teamNo}) => {
     useEffect(() => {
         console.log(`${teamNo} 참가자 1`);
         console.log(player1);
-    }, [player1.memberNo]);
+    }, [player1]);
     useEffect(() => {
         console.log(`${teamNo} 참가자 2`);
         console.log(player2);
-    }, [player2.memberNo]);
+    }, [player2]);
     return (
         <div className="flex flex-col bg-primaryDark w-[414px] h-[416px] rounded-[25px] p-5 justify-around items-center">
             <div className="text-white text-[48px] font-bold">Team {teamNo}</div>
-            {!player1.memberNo ? <AddPlayerButton /> : <PlayerProfile player={player1} />}
-            {!player2.memberNo ? <AddPlayerButton /> : <PlayerProfile player={player2} />}
+            { (player1 && player1.memberNo) ? <PlayerProfile player={player1} /> : <AddPlayerButton /> }
+            { (player2 && player2.memberNo) ? <PlayerProfile player={player2} /> : <AddPlayerButton /> }
         </div>
     );
 };
