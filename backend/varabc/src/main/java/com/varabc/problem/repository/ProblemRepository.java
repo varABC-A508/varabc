760a2dfc6828cf1ff7ce4d0729cb3934765dbfa6
오준석 <1004joonsuk@naver.com>
2023-08-12 17:22:43 +0900

    Feature/search api API 구현 완료 mr

--- backend/varabc/src/main/java/com/varabc/problem/repository/ProblemRepository.java
+++ backend/varabc/src/main/java/com/varabc/problem/repository/ProblemRepository.java
@@ -18,4 +18,9 @@ public interface ProblemRepository extends JpaRepository<Problem,Long> {
     @Query("SELECT p.problemNo FROM Problem p WHERE p.problemSource = :source AND p.problemLevel LIKE :level%")
     List<Long> getList(@Param("source") String problemSource, @Param("level") String problemLevel);
 
+    List<Problem> findByProblemTitleContainingAndProblemAlgorithmType(String keyword, String compareBit);
+    List<Problem> findByProblemTitleContaining(String keyword);
+    List<Problem> findByProblemNo(long problemNo);
+    List<Problem> findByProblemNoAndProblemAlgorithmType(long problemNo, String compareBit);
+    List<Problem> findByProblemAlgorithmType(String compareBit);
 }
