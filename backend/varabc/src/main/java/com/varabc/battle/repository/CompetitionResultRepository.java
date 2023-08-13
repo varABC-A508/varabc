5297bed3270b8503f6e683476c590976e5104bb6
heejo <heejo515@naver.com>
2023-08-13 21:47:37 +0900

    fix/mypage-battle-list: 배틀리스트 수정

--- backend/varabc/src/main/java/com/varabc/battle/repository/CompetitionResultRepository.java
+++ backend/varabc/src/main/java/com/varabc/battle/repository/CompetitionResultRepository.java
@@ -6,9 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CompetitionResultRepository extends JpaRepository<CompetitionResult, Long> {
 
-    List<CompetitionResult> findByCompetitionResultT1M1NoOrCompetitionResultT1M2No(Long competitionResultT1M1No, Long competitionResultT1M2No);
+    List<CompetitionResult> findByCompetitionResultT1M1NoOrCompetitionResultT1M2NoOrCompetitionResultT2M1NoOrCompetitionResultT2M2No(Long competitionResultT1M1No, Long competitionResultT1M2No,Long competitionResultT2M1No, Long competitionResultT2M2No);
 
-    List<CompetitionResult> findByCompetitionResultT2M1NoOrCompetitionResultT2M2No(Long competitionResultT2M1No, Long competitionResultT2M2No);
 
     CompetitionResult findByCompetitionResultNo(Long competitionResultNo);
 }
