5d449ce2c41299512c96f410089a2ccc8fd19f64
이예린 <h2002y@naver.com>
2023-08-14 00:10:02 +0900

    test: socketInstance forceNew false 추가

--- frontend/varabc/src/modules/socketInstance.js
+++ frontend/varabc/src/modules/socketInstance.js
@@ -4,5 +4,6 @@ const socket = io('https://varabc.com:3001', {
   reconnectionAttempts: 5, // 최대 5번 재연결 시도
   reconnectionDelay: 1000, // 1초마다 재연결 시도
   reconnectionDelayMax: 5000, // 최대 5초의 재연결 간격
+  forceNew: false
 });
 export default socket;
