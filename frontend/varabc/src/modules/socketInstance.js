94f3870747c14d5437c470c76f24d5f1bd224341
이예린 <h2002y@naver.com>
2023-08-13 23:53:16 +0900

    test: socketInstance port 번복

--- frontend/varabc/src/modules/socketInstance.js
+++ frontend/varabc/src/modules/socketInstance.js
@@ -1,5 +1,5 @@
 import {io} from 'socket.io-client';
-const socket = io('https://varabc.com', {
+const socket = io('https://varabc.com:3001', {
   reconnection: true, // 자동 재연결 활성화
   reconnectionAttempts: 5, // 최대 5번 재연결 시도
   reconnectionDelay: 1000, // 1초마다 재연결 시도
