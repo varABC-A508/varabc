61b44c3afa9719fb198b9addb534a21f920f7a90
오정빈 <ohjb93@gmail.com>
2023-08-14 13:16:44 +0900

    Feature/practice datacheck

--- frontend/varabc/src/components/ide/IdeContainer.js
+++ frontend/varabc/src/components/ide/IdeContainer.js
@@ -2,19 +2,42 @@ import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 import Ide from ".";
 import Problem from "./Problem";
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function IdeContainer() {
   const params = useParams();
   const problemNo = params.problemNo;
+  const [memberNo, setMemberNo] = useState('');
+  const accessToken = sessionStorage.getItem('access-token');
+
+  const getMemberNo = () => {
+    axios.get("https://varabc.com:8080/member/getUserInfo", {
+      headers: {
+        "access-token": accessToken
+      }
+    }).then((res) => {
+      console.log(">>>>멤버 정보 요청", res);
+      setMemberNo(res.userInfo.memberNo);
+      console.log(result.output);
+    }).catch(function (err) {
+      alert("멤버 정보 요청\n" + err);
+    });
+  }
+
+  useEffect(() => {
+    getMemberNo();
+  }
+  );
+
   return (
     <PanelGroup direction="horizontal">
-        <Panel defaultSize={30} style={{overflowY:"auto"}}>
-            <Problem problemNo={problemNo} />
-        </Panel>
-        <PanelResizeHandle className="cursor-col-resize" style={{ width: '4px', backgroundColor:'gray' }} />
-        <Panel defaultSize={70}>
-          <Ide className="w-full" problemNo={problemNo} />
-        </Panel>
+      <Panel defaultSize={30} style={{ overflowY: "auto" }}>
+        <Problem problemNo={problemNo} />
+      </Panel>
+      <PanelResizeHandle className="cursor-col-resize" style={{ width: '4px', backgroundColor: 'gray' }} />
+      <Panel defaultSize={70}>
+        <Ide className="w-full" problemNo={problemNo} memberNo={memberNo} />
+      </Panel>
     </PanelGroup>
   );
 }
