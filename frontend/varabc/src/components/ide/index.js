71b9f2046eecf5329c250c8dba32407ae32f0cf3
h2002y <h2002y@naver.com>
2023-08-14 16:41:30 +0900

    fix: ide output 출력 메세지 변경

--- frontend/varabc/src/components/ide/index.js
+++ frontend/varabc/src/components/ide/index.js
@@ -174,7 +174,12 @@ const Ide = ( { problemNo }) => {
             <div>실행 결과</div>
             <div>실행 시간: {result.executionTime}</div>
             <div>사용 메모리: {result.memoryUsage}</div>
-            <div>{result.result === 1 ? "성공" : "실패"}</div>
+            {/* <div>{!result.output && result.result === 1 ? "성공" : "실패"}</div> */}
+            { <div>{(!result.output) ? (result.result === 1 ? "성공" : "실패") : ("")}</div> }
+            <br />
+            {result.output.forEach((outputMessage, index) => {
+              return(<div>{`테스트케이스 ${index}: ${outputMessage}`}</div>);
+            })}
             { result.output ? (<div>output : {result.output} </div>): <div>제출 되었습니다.</div>}
           </Panel>
           <PanelResizeHandle className="cursor-row-resize bg-primaryDark" style={{ height: '4px', backgroundColor: 'gray' }} />
