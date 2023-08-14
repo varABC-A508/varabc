7f0987176c1c5cd89668705afc9e2a7b805b52cb
h2002y <h2002y@naver.com>
2023-08-14 13:46:11 +0900

    fix: 게임 로직 수정

--- frontend/varabc/src/components/ide/IdeContainer.js
+++ frontend/varabc/src/components/ide/IdeContainer.js
@@ -2,13 +2,10 @@ import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 import Ide from ".";
 import Problem from "./Problem";
 import { useParams } from "react-router-dom";
-import { useState } from "react";
 
 function IdeContainer() {
   const params = useParams();
   const problemNo = params.problemNo;
-  const [memberNo, setMemberNo] = useState('');
-  const accessToken = sessionStorage.getItem('access-token');
 
   return (
     <PanelGroup direction="horizontal">
