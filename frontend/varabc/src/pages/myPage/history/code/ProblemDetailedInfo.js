fe38d3df8d86c2ec4868b97e2a8a3119e60459d0
@iwri2089 <iwri2089@gmail.com>
2023-08-15 21:57:50 +0900

    fix: my code 문제 정보 이미지/testcase fix

--- frontend/varabc/src/pages/myPage/history/code/ProblemDetailedInfo.js
+++ frontend/varabc/src/pages/myPage/history/code/ProblemDetailedInfo.js
@@ -22,7 +22,7 @@ const theme = createTheme({
   },
 });
 
-const ProblemDetailedInfo = ({ problemData }) => {
+const ProblemDetailedInfo = ({ problemData, problemContent, problemInputContent, problemOutputContent }) => {
   return (
     <ThemeProvider theme={theme}>
       <div className="mb-3 bg-primaryDark">
@@ -32,21 +32,15 @@ const ProblemDetailedInfo = ({ problemData }) => {
           </AccordionSummary>
           <AccordionDetails sx={{ bgcolor: "secondary.main", color: 'white' }}>
             <div className="p-4">
-              <div className="text-[24px] mb-1 font-bold whitespace-pre-wrap">
-                {problemData.problemNo}. {problemData.problemTitle}
-              </div>
-              <div className="text-[20px] mb-5 font-bold">
-                {problemData.problemLevel}
-              </div>
               <Title text="문제 설명"></Title>
-              <Content content={problemData.problemContent}></Content>
+              <Content content={problemContent}></Content>
               <Title text="입력"></Title>
-              <Content content={problemData.problemInputContent}></Content>
+              <Content content={problemInputContent}></Content>
               <Title text="출력"></Title>
-              <Content content={problemData.problemOutputContent}></Content>
+              <Content content={problemOutputContent}></Content>
               <Testcase
-                inputUrlList={problemData.testCaseInputPublicList}
-                outputUrlList={problemData.testCaseOutputPublicList}
+                inputUrlList={problemData.testCaseInputList}
+                outputUrlList={problemData.testCaseOutputList}
               />
             </div>
           </AccordionDetails>
