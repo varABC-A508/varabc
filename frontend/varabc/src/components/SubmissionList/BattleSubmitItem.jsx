7a2e4bee4c96b0f96513faa0dcdfe210788bccf1
h2002y <h2002y@naver.com>
2023-08-16 14:02:38 +0900

    fix: 게임 서버 로직 수정

--- frontend/varabc/src/components/SubmissionList/BattleSubmitItem.jsx
+++ frontend/varabc/src/components/SubmissionList/BattleSubmitItem.jsx
@@ -1,17 +1,20 @@
 const BattleSubmitItem = ({result}) => {
+
   const cellText = "p-3 text-white flex justify-center items-center text-center";
-    const resultColor = (result.submitStatus === "맞았습니다." ? "text-green" : "text-red");
+  const resultColor = (result.submitStatus === "맞았습니다." ? "text-green" : "text-red");
   
-    return (
-      <tr className="grid grid-cols-12">
-        <td className={`col-span-4 ${cellText}`}>{result.memberNickname}</td>
-        <td className={`col-span-2 ${cellText} ${resultColor}`}>{result.submitStatus}</td>
-        <td className={`${cellText}`}>{result.submitUsedMemory}KB</td>
-        <td className={`${cellText}`}>{result.submitUsedTime}ms</td>
-        <td className={`${cellText}`}>{result.submitLanguage}</td>
-        <td className={`col-span-3 ${cellText}`}>{result.submitTime}</td>
-      </tr>
-    );
+  return (
+    <tr className="grid grid-cols-12">
+      <td className={`col-span-4 ${cellText}`}>{result.memberNickname}</td>
+      <td className={`col-span-2 ${cellText} ${resultColor}`}>
+        {result.submitStatus}
+      </td>
+      <td className={`${cellText}`}>{result.submitUsedMemory}KB</td>
+      <td className={`${cellText}`}>{result.submitUsedTime}ms</td>
+      <td className={`${cellText}`}>{result.submitLanguage}</td>
+      <td className={`col-span-3 ${cellText}`}>{result.submitTime}</td>
+    </tr>
+  );
   }; 
   
   export default BattleSubmitItem; 
