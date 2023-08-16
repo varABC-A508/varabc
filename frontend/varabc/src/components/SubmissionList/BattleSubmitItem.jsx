b7bb282df3ce5e25ab5b06f3ad11e5c9eb30bab5
h2002y <h2002y@naver.com>
2023-08-16 13:49:10 +0900

    fix: 게임 서버 로직 수정

--- frontend/varabc/src/components/SubmissionList/BattleSubmitItem.jsx
+++ frontend/varabc/src/components/SubmissionList/BattleSubmitItem.jsx
@@ -1,6 +1,6 @@
 const BattleSubmitItem = ({result}) => {
   const cellText = "p-3 text-white flex justify-center items-center text-center";
-    const resultColor = (result.submitStatus == "맞았습니다." ? "text-green" : "text-red");
+    const resultColor = (result.submitStatus === "맞았습니다." ? "text-green" : "text-red");
   
     return (
       <tr className="grid grid-cols-12">
