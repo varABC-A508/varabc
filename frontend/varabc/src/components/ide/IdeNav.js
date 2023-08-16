f9504c56453a433140d470d493e6555a4330a792
이예린 <h2002y@naver.com>
2023-08-17 00:06:30 +0900

    fix: 배틀 코드 작성 차례를 알려주는 기능? 반작성

--- frontend/varabc/src/components/ide/IdeNav.js
+++ frontend/varabc/src/components/ide/IdeNav.js
@@ -16,6 +16,7 @@ const IdeNav = () => {
             <FontAwesomeIcon className='ml-4 text-white' icon={faBookOpen} />
             <FontAwesomeIcon className='ml-4 text-white' icon={faMicrophone} />
           </div>
+          <div>{JSON.parse(sessionStorage.getItem('isPlayerTurn')) ? "나의 턴" : "페어의 턴"}</div>
           <div className='w-70% bg-primary text-white p-1 flex flex-wrap justify-between'>
             {isPractice ? null : <Timer />}
             <IdeMode />
