3f95bad6b231e183af90246eadee3f1584b1c6b8
이예린 <h2002y@naver.com>
2023-08-18 02:10:20 +0900

    design: 초대 url 알림 디자인 수정

--- frontend/varabc/src/components/common/Button/InviteLinkButton.jsx
+++ frontend/varabc/src/components/common/Button/InviteLinkButton.jsx
@@ -1,40 +1,49 @@
 import { useState } from "react";
+import { Alert } from '@mui/material';
+import { Snackbar } from '@mui/material';
 
 const InviteLinkButton = () => {
 
-  const [inviteMessage, setInviteMessage] = useState('');
-  const [showMessage, setShowMessage] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const onButtonClick = () => {
+    const currentUrl = window.location.href;
+    copyToClipboard(currentUrl);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
 
   const copyToClipboard = (text) => {
     navigator.clipboard
       .writeText(text)
       .then(() => {
-        setInviteMessage('URL이 클립보드에 복사되었습니다!');
-        setShowMessage(true);
-
-        setTimeout(() => {
-          setShowMessage(false);
-        }, 3000); // 3초 후에 알림 메시지가 사라짐
+        setOpen(true);
       })
       .catch((error) => {
-        console.error('클립보드 복사 실패:', error);
+        alert('클립보드 복사 실패:', error);
       });
-  };
-    
-  const handleCopyUrl = () => {
-    const currentUrl = window.location.href;
-    copyToClipboard(currentUrl);
   };
 
   return (
-    <button
-      onClick={handleCopyUrl}
-      type="button"
-      className={`bg-white text-primary flex justify-center items-center rounded-lg font-bold w-[358px] h-[100px] text-[64px]`}
-    >
-      {showMessage && <div className="notification">{inviteMessage}</div>}
-      초대 URL
-    </button>
+    <>
+      <button
+        onClick={onButtonClick}
+        type="button"
+        className={`bg-white text-primary flex justify-center items-center rounded-lg font-bold w-[358px] h-[100px] text-[64px]`}
+      >
+        초대 URL
+      </button>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+          초대 URL 클립보드 복사 성공!
+        </Alert>
+      </Snackbar>
+    </>
   );
 };
 
