564c0885924d7ac0bec35c8567b5a9c49e103090
h2002y <h2002y@naver.com>
2023-08-17 21:01:33 +0900

    design: css 변경

--- frontend/varabc/src/components/Review/Review.jsx
+++ frontend/varabc/src/components/Review/Review.jsx
@@ -1,6 +1,4 @@
 // 리뷰 페이지 (임시)
-import {useDispatch, useSelector} from 'react-redux';
-import { setReadability, setNaming, setSpeed, setCommunication } from '../../redux/Reducer/reviewReducers';
 import ReviewTag from "./ReviewTag.jsx";
 
 export default function Review() {
@@ -11,11 +9,6 @@ export default function Review() {
     { index: 2, content: "변수명이 쉬워요" },
     { index: 3, content: "소통을 잘해요" },
   ];
-  const dispatch = useDispatch();
-  const readability = useSelector((state) => state.review.readability);
-  const naming = useSelector((state) => state.review.naming);
-  const speed = useSelector((state) => state.review.speed);
-  const communication = useSelector((state) => state.review.communication);
 
   const reviewTags = TAGS.map((tag) => {
     return (
@@ -27,27 +20,6 @@ export default function Review() {
       />
     );
   });
-
-  const handleTagClick = (index) => {
-    switch (parseInt(index)) {
-      case 0:
-        dispatch(setReadability(!readability));
-        break;
-      case 1:
-        dispatch(setNaming(!naming));
-        break;
-      case 2:
-        dispatch(setSpeed(!speed));
-        break;
-      case 3:
-        dispatch(
-          setCommunication(!communication)
-        );
-        break;
-      default:
-        break;
-    }
-  };
 
   return <div>{reviewTags}</div>;
 }
--- frontend/varabc/src/components/Review/ReviewTag.jsx
+++ frontend/varabc/src/components/Review/ReviewTag.jsx
@@ -1,8 +1,43 @@
+import {useDispatch, useSelector} from 'react-redux';
+import { setReadability, setNaming, setSpeed, setCommunication } from '../../redux/Reducer/reviewReducers';
+import { useState } from 'react';
+
 export default function ReviewTag({ index, content, clicked, handleTagClick }) {
+
+  const dispatch = useDispatch();
+  const readability = useSelector((state) => state.review.readability);
+  const naming = useSelector((state) => state.review.naming);
+  const speed = useSelector((state) => state.review.speed);
+  const communication = useSelector((state) => state.review.communication);
+
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleTagClick = (index) => {
+    setIsClicked((prev) => !prev);
+    switch (parseInt(index)) {
+      case 0:
+        dispatch(setReadability(!readability));
+        break;
+      case 1:
+        dispatch(setNaming(!naming));
+        break;
+      case 2:
+        dispatch(setSpeed(!speed));
+        break;
+      case 3:
+        dispatch(
+          setCommunication(!communication)
+        );
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <div
       key={index}
-      className={`inline-flex justify-center items-center m-3 w-[254px] h-[64px] rounded-[30px] cursor-pointer ${clicked ? "bg-point": "bg-white" }`}
+      className={`inline-flex justify-center items-center m-3 w-[254px] h-[64px] rounded-[30px] cursor-pointer ${isClicked ? "bg-point": "bg-white" }`}
       onClick={handleTagClick}
     >
       <span className="text-[#2E2E2E] text-2xl font-bold"># {content}</span>
