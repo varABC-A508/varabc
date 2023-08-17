97eb5700f38c2b15d4d271f3842f36df6ca96a90
GardenJB <ohjb93@gmail.com>
2023-08-17 16:38:24 +0900

    fix/bgcolor : 배틀 모드 수정, IDE 문제창 배경색 변경

--- frontend/varabc/src/components/ide/index.js
+++ frontend/varabc/src/components/ide/index.js
@@ -69,7 +69,7 @@ const Ide = ( { problemNo }) => {
         setMemberNo(res.data.userInfo.memberNo);
         sessionStorage.setItem('memberNo', res.data.userInfo.memberNo);
       }).catch((err) => {
-        swal ( "이런" ,  "서버에 문제가 생겼습니다! 나중에 다시 시도해주세요!>8" + err ,  "error" );
+        swal ( "이런" ,  "로그인 후 사용해 주세요!>8" + err ,  "error" );
         navigate("/");
       });
 
