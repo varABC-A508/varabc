e82d55bd24bea55d49235f11b91fe689a5cbfc60
h2002y <h2002y@naver.com>
2023-08-17 17:55:40 +0900

    design: css 변경

--- frontend/varabc/src/components/ide/Problem/Content.js
+++ frontend/varabc/src/components/ide/Problem/Content.js
@@ -2,7 +2,7 @@ const DescriptionContent = ({ content }) => {
   return (
     <div
       className="text-[16px] whitespace-pre-wrap scrollbar mt-2 mb-8 pl-2 pr-2 text-white bg-primaryDark"
-    >
+    >{content}
     </div>
   );
 };
